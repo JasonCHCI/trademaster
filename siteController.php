7f78a882929817d31091da2faf3bff3640901398
jamesdong89 <jamesdong89@gmail.com>
2017-10-13 14:19:21 -0700

    membershio

--- siteController.php
+++ siteController.php
@@ -9,39 +9,55 @@ $action = $_GET['action'];
 $pc = new SiteController();
 $pc->route($action);
 
-class SiteController {
+class SiteController
+{
 
-	// route us to the appropriate class method for this action
-	public function route($action) {
-		switch($action) {
-			case 'welcome':
-			$this->welcome();
-			break;
+    // route us to the appropriate class method for this action
+    public function route($action)
+    {
+        switch ($action) {
+            case 'welcome':
+                $this->welcome();
+                break;
 
-			case 'home':
-			$this->home();
-			break;
+            case 'home':
+                $this->home();
+                break;
 
-			// redirect to home page if all else fails
-			default:
-			header('Location: '.BASE_URL);
-			exit();
+            case 'membership':
+                $this->membership();
+                break;
 
-		}
+            // redirect to home page if all else fails
+            default:
+                header('Location: ' . BASE_URL);
+                exit();
 
-	}
+        }
 
-	public function welcome() {
-		$pageName = 'Welcome';
-		include_once SYSTEM_PATH.'/view/header.html';
-		include_once SYSTEM_PATH.'/view/welcome.html';
-		include_once SYSTEM_PATH.'/view/footer.html';
-	}
+    }
 
-	public function home() {
-		$pageName = 'Home';
-		include_once SYSTEM_PATH.'/view/header.html';
-		include_once SYSTEM_PATH.'/view/home.html';
-		include_once SYSTEM_PATH.'/view/footer.html';
-	}
+    public function welcome()
+    {
+        $pageName = 'Welcome';
+        include_once SYSTEM_PATH . '/view/header.html';
+        include_once SYSTEM_PATH . '/view/welcome.html';
+        include_once SYSTEM_PATH . '/view/footer.html';
+    }
+
+    public function home()
+    {
+        $pageName = 'Home';
+        include_once SYSTEM_PATH . '/view/header.html';
+        include_once SYSTEM_PATH . '/view/home.html';
+        include_once SYSTEM_PATH . '/view/footer.html';
+    }
+
+    public function membership()
+    {
+        $pageName = 'Membership';
+        include_once SYSTEM_PATH . '/view/header.html';
+        include_once SYSTEM_PATH . '/view/membership.html';
+        include_once SYSTEM_PATH . '/view/footer.html';
+    }
 }
